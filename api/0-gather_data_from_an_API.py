cf2c344a1acf48e8d9f9260cdd7226d3ab192ceb
alexeychern0v <alexchernov063@gmail.com>
2024-04-19 19:32:00 +0200

    added perm

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,18 +8,14 @@ def callapi(id):
     """Calls the api, parses data and prints result"""
     url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id)
     req = requests.get(url, params={"_expand": "user"})
-    data = req.json()
-    
+    data = req.json() 
     TASK_TITLE = [task["title"] for task in data if task["completed"]]
     EMPLOYEE_NAME = data[0]["user"]["name"]
     NUMBER_OF_DONE_TASKS = len(TASK_TITLE)
     TOTAL_NUMBER_OF_TASKS = len(data)
-    
     print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-    
     for task_title in TASK_TITLE:
         print("\t {}".format(task_title))
-
 
 if __name__ == "__main__":
     """Main function"""
